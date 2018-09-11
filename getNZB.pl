7fbf43703e5eb9117470960819046a33740876b2
Tom <mail@fraglimit.de>
2018-09-12 00:10:48 +0200

    Version 1.0

--- getNZB.pl
+++ getNZB.pl
@@ -2,29 +2,40 @@
 
 use strict;
 use warnings;
+use XML::RSS::Parser;
+use FileHandle;
 
+
+########### Get the RSS File #############
 my $tempfile = "/tmp/output.xml";
 
 print "Host: $ARGV[0]\n";
 print "TempFile: $tempfile\n";
+system ("torify wget --no-check-certificate \"$ARGV[0]\" -O $tempfile");
 
-# system ("torify wget --no-check-certificate \"$ARGV[0]\" -O $tempfile");
+ ########### Process RSS File #############
+my $p = XML::RSS::Parser->new;
+my $fh = FileHandle->new($tempfile);
+my $feed = $p->parse_file($fh);
+ 
+my $feed_title = $feed->query('/channel/title');
+print $feed_title->text_content;
+my $count = $feed->item_count;
+print " ($count)\n";
 
-my @links = ();
-my @names =();
-open FILE, '<', $tempfile or die "can't open $tempfile. $!\n";
-
-while (my $zeile = <FILE>) {
-	if ($zeile =~ /title/)
-	{	
-		print $zeile;
-		push @names, $zeile;
-	}
+########### Get the NZBs #############
+foreach my $i ( $feed->query('//item') ) { 
+    my $title = $i->query('title');
+    #print $title->text_content;
+	my $link = $i->query('link');
+    #print $link->text_content;
+    print "\n==========Next file==========\n"; 
+	
+	if (-e "/tmp/".$title->text_content.".nzb") {
+			print "Skipping file, already Exist: /tmp/".$title->text_content.".nzb\n";
+		} else {
+			print "Downloading ...\n";
+			system ("torify wget --no-check-certificate \"".$link->text_content."\" -O /tmp/".$title->text_content.".nzb");
+		}
 }
-close FILE;
-
-
-
-
-
 exit (0);
