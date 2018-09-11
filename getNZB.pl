01aefc9c8257e06007d1e0edc26a522ec527ea3a
Tom <mail@fraglimit.de>
2018-09-11 22:46:17 +0200

    initial

--- getNZB.pl
+++ getNZB.pl
@@ -3,5 +3,28 @@
 use strict;
 use warnings;
 
-system ("torify", "wget --no-check-certificate " $ARGV[1] " -O /tmp/output.xml");
- 
+my $tempfile = "/tmp/output.xml";
+
+print "Host: $ARGV[0]\n";
+print "TempFile: $tempfile\n";
+
+# system ("torify wget --no-check-certificate \"$ARGV[0]\" -O $tempfile");
+
+my @links = ();
+my @names =();
+open FILE, '<', $tempfile or die "can't open $tempfile. $!\n";
+
+while (my $zeile = <FILE>) {
+	if ($zeile =~ /title/)
+	{	
+		print $zeile;
+		push @names, $zeile;
+	}
+}
+close FILE;
+
+
+
+
+
+exit (0);
